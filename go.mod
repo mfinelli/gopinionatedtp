2ce1253c6a8257ca385744d1af3fd85cf24a421a
Mario Finelli <mario@finel.li>
2023-09-17 18:28:18 +0200

    Add terminal-based QR code printer

--- go.mod
+++ go.mod
@@ -3,13 +3,27 @@ module go.finelli.dev/gopinionatedtp
 go 1.21
 
 require (
+	github.com/pterm/pterm v0.12.69
 	github.com/stretchr/testify v1.8.4
+	github.com/yeqown/go-qrcode/v2 v2.2.2
 	golang.org/x/crypto v0.13.0
 )
 
 require (
+	atomicgo.dev/cursor v0.2.0 // indirect
+	atomicgo.dev/keyboard v0.2.9 // indirect
+	atomicgo.dev/schedule v0.1.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gookit/color v1.5.4 // indirect
+	github.com/lithammer/fuzzysearch v1.1.8 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	github.com/yeqown/reedsolomon v1.0.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/term v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ terminal.go
@@ -0,0 +1,105 @@
+/*!
+ * Copyright 2023 Mario Finelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package otp
+
+import (
+	"fmt"
+
+	"github.com/pterm/pterm"
+	"github.com/yeqown/go-qrcode/v2"
+)
+
+// QrCodeToTerminal constructs a TOTP URI and then outputs a terminal-based
+// (using pterm) QR code that can be scanned by an authentication app.
+func QrCodeToTerminal(username, issuer, secret string) error {
+	uri := BuildTotpUri(username, issuer, secret)
+	qrc, _ := qrcode.New(uri)
+
+	w := newTerminalWriter()
+
+	if err := qrc.Save(w); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// implements a terminal writer using pterm for qrcode
+// https://github.com/yeqown/go-qrcode/tree/main/writer
+var _ qrcode.Writer = (*terminalWriter)(nil)
+
+type terminalWriter struct{}
+
+func newTerminalWriter() *terminalWriter {
+	w := &terminalWriter{}
+	return w
+}
+
+func (w terminalWriter) Write(mat qrcode.Matrix) error {
+	chr := "██"
+
+	ww, _ := mat.Width(), mat.Height()
+	wwWithBorder := ww + 4
+
+	mat.Iterate(qrcode.IterDirection_COLUMN,
+		func(x int, y int, state qrcode.QRValue) {
+			if x == 0 && y == 0 {
+				// add the top row border
+				for i := 0; i < wwWithBorder; i++ {
+					pterm.FgWhite.Printf(chr)
+				}
+				fmt.Println()
+				for i := 0; i < wwWithBorder; i++ {
+					pterm.FgWhite.Printf(chr)
+				}
+				fmt.Println()
+			}
+
+			if y == 0 {
+				pterm.FgWhite.Printf(chr)
+				pterm.FgWhite.Printf(chr)
+			}
+
+			if state.IsSet() {
+				pterm.FgBlack.Printf(chr)
+			} else {
+				pterm.FgWhite.Printf(chr)
+			}
+
+			if y == ww-1 {
+				pterm.FgWhite.Printf(chr)
+				pterm.FgWhite.Printf(chr)
+				fmt.Println()
+			}
+		})
+
+	// add the bottom row border
+	for i := 0; i < wwWithBorder; i++ {
+		pterm.FgWhite.Printf(chr)
+	}
+	fmt.Println()
+	for i := 0; i < wwWithBorder; i++ {
+		pterm.FgWhite.Printf(chr)
+	}
+	fmt.Println()
+
+	return nil
+}
+
+func (w terminalWriter) Close() error {
+	return nil
+}
